878f3f418559325d48c425e449ae9a672e79a2ff
FuryOhFury <needsomespaceforit2@gmail.com>
2025-04-14 21:08:45 +0500

    feat/add cell and celllist

---
+++ Assets/Game/GameEngine/Cells/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+	[Serializable]
+	public sealed class Cell
+	{
+		public string ID => _id;
+
+		public Sprite Icon => _icon;
+
+		[SerializeField]
+		private string _id;
+		[SerializeField]
+		private Sprite _icon;
+	}
+}
---
+++ Assets/Game/GameEngine/Cells/CellList.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	public sealed class CellList : ScriptableObject
+	{
+		public Cell[] Cells => _cells;
+		
+		[SerializeField]
+		private Cell[] _cells;
+	}
+}
