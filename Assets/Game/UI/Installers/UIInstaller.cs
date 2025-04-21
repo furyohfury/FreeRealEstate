754e592417447260af1e4a6fb7a71e9be73fbeb0
FuryOhFury <needsomespaceforit2@gmail.com>
2025-04-21 19:37:24 +0500

    feat/add ui of target cell

---
+++ Assets/Game/UI/Installers/UIInstaller.cs
@@ -0,0 +1,19 @@
+﻿using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace UI
+{
+	public sealed class UIInstaller : MonoInstaller
+	{
+		[SerializeField]
+		private TMP_Text _targetCellText;
+		
+		public override void InstallBindings()
+		{
+			Container.BindInterfacesAndSelfTo<TargetCellTextPresenter>()
+			         .AsSingle()
+			         .WithArguments(_targetCellText);
+		}
+	}
+}
---
+++ Assets/Game/UI/Presenters/TargetCellTextPresenter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using DG.Tweening;
+using Game;
+using R3;
+using TMPro;
+using Zenject;
+
+namespace UI
+{
+	public sealed class TargetCellTextPresenter : IInitializable, IDisposable
+	{
+		private readonly CellChooser _cellChooser;
+		private ActiveBundleService _activeBundleService;
+		private readonly TMP_Text _tmpText;
+
+		private readonly CompositeDisposable _disposable = new();
+
+		public TargetCellTextPresenter(TMP_Text tmpText, ActiveBundleService activeBundleService, CellChooser cellChooser)
+		{
+			_tmpText = tmpText;
+			_activeBundleService = activeBundleService;
+			_cellChooser = cellChooser;
+		}
+
+		public void Initialize()
+		{
+			_cellChooser.DesiredCell
+			            .Subscribe(OnCellChanged)
+			            .AddTo(_disposable);
+
+			_activeBundleService.OnLevelEnded
+			                    .Subscribe(OnLevelEnded)
+			                    .AddTo(_disposable);
+
+			FadeInText();
+		}
+
+		private void FadeInText()
+		{
+			var color = _tmpText.color;
+			color.a = 0;
+			_tmpText.color = color;
+			_tmpText.DOFade(1, 3);
+		}
+
+		private void OnCellChanged(Cell cell)
+		{
+			_tmpText.text = $"Find {cell.ID}";
+		}
+
+		private void OnLevelEnded(Unit _)
+		{
+			_tmpText.text = string.Empty;
+		}
+
+		public void Dispose()
+		{
+			_disposable.Clear();
+		}
+	}
+}
