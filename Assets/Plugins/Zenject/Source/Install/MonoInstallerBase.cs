7da0b0d38c42155f631341ffc43a9051b1790221
FuryOhFury <needsomespaceforit2@gmail.com>
2025-04-26 14:57:47 +0500

    reimport zenject and odin

---
+++ Assets/Plugins/Zenject/Source/Install/MonoInstallerBase.cs
@@ -0,0 +1,39 @@
+#if !NOT_UNITY3D
+
+using System;
+using System.Diagnostics;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Zenject
+{
+    // We'd prefer to make this abstract but Unity 5.3.5 has a bug where references
+    // can get lost during compile errors for classes that are abstract
+    [DebuggerStepThrough]
+    public class MonoInstallerBase : SerializedMonoBehaviour, IInstaller
+    {
+        [Inject]
+        protected DiContainer Container
+        {
+            get; set;
+        }
+
+        public virtual bool IsEnabled
+        {
+            get { return enabled; }
+        }
+
+        public virtual void Start()
+        {
+            // Define this method so we expose the enabled check box
+        }
+
+        public virtual void InstallBindings()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+#endif
+
