806398d114bd6a117f661fddc3b7e68b447a15e3
FuryOhFury <needsomespaceforit2@gmail.com>
2025-05-02 14:23:20 +0500

    feat/add core components

---
+++ Assets/Game/GameEngine/Components/Content/MoveComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Game;
+using UnityEngine;
+
+namespace GameEngine
+{
+	[Serializable]
+	public sealed class MoveComponent : IComponent
+	{
+		[SerializeField]
+		private Rigidbody _rigidbody;
+		[SerializeField]
+		private Transform _transform;
+		[SerializeField]
+		private float _moveSpeed;
+
+		public AndCondition CanMove = new();
+
+		public void Move(Vector3 direction)
+		{
+			if (direction == Vector3.zero || CanMove.Invoke())
+			{
+				return;
+			}
+			
+			var delta = _transform.forward * direction.z + _transform.right * direction.x;
+			_rigidbody.MovePosition(_transform.position + delta * _moveSpeed);
+		}
+	}
+}
---
+++ Assets/Game/GameEngine/Components/Content/RotateComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Game;
+using UnityEngine;
+
+namespace GameEngine
+{
+	[Serializable]
+	public sealed class RotateComponent : IComponent
+	{
+		public AndCondition CanRotate = new();
+		[SerializeField]
+		private float _rotateSpeed;
+		[SerializeField]
+		private Rigidbody _rigidbody;
+
+		public void Rotate(Vector3 direction)
+		{
+			if (CanRotate.Invoke() == false)
+			{
+				return;
+			}
+			
+			var rotation = _rigidbody.rotation;
+			_rigidbody.MoveRotation(Quaternion.Lerp(rotation, rotation * Quaternion.Euler(direction), _rotateSpeed));
+		}
+	}
+}
