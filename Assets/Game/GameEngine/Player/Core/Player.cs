61589992275540db5ad0056c65fa4f7411e1e648
FuryOhFury <needsomespaceforit2@gmail.com>
2025-05-02 14:23:40 +0500

    feat/add player and its controller

---
+++ Assets/Game/GameEngine/Player/Core/Player.cs
@@ -0,0 +1,25 @@
+﻿using GameEngine;
+using UnityEngine;
+
+namespace Game
+{
+	public sealed class Player : Entity
+	{
+		[SerializeField]
+		private MoveComponent _moveComponent;
+		[SerializeField]
+		private LifeComponent _lifeComponent;
+		[SerializeField]
+		private RotateComponent _rotateComponent;
+
+		private void Awake()
+		{
+			_moveComponent.CanMove.AddCondition(() => _lifeComponent.IsAlive);
+			_rotateComponent.CanRotate.AddCondition(() => _lifeComponent.IsAlive);
+			
+			AddComponent(_moveComponent);
+			AddComponent(_lifeComponent);
+			AddComponent(_rotateComponent);
+		}
+	}
+}
---
+++ Assets/Game/GameEngine/Player/PlayerMoveController.cs
@@ -0,0 +1,45 @@
+﻿using GameEngine;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+	public sealed class PlayerMoveController : IInitializable, IFixedTickable
+	{
+		private readonly PlayerService _playerService;
+		private Entity _player;
+		private MoveComponent _moveComponent;
+		private readonly PlayerInputReader _playerInputReader;
+		private RotateComponent _rotateComponent;
+
+		public PlayerMoveController(PlayerService playerService, PlayerInputReader playerInputReader)
+		{
+			_playerService = playerService;
+			_playerInputReader = playerInputReader;
+		}
+
+		public void Initialize()
+		{
+			_player = _playerService.Player;
+			_moveComponent = _player.GetComponent<MoveComponent>();
+			_rotateComponent = _player.GetComponent<RotateComponent>();
+			_playerInputReader.OnLookAction += OnLook;
+		}
+		public void FixedTick()
+		{
+			Move();
+		}
+
+		private void Move()
+		{
+			var moveDirection = _playerInputReader.MoveDirection;
+			_moveComponent.Move(moveDirection * Time.deltaTime);
+		}
+
+		private void OnLook(Vector2 direction)
+		{
+			var horizontalRotation = new Vector3(0, direction.x, 0);
+			_rotateComponent.Rotate(horizontalRotation);
+		}
+	}
+}
