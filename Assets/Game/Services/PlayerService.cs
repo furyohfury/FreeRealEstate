4b76cd49ed7a13b840e26d6ba704893eb0ec6618
FuryOhFury <needsomespaceforit2@gmail.com>
2025-05-02 14:24:23 +0500

    feat/add player service

---
+++ Assets/Game/Services/PlayerService.cs
@@ -0,0 +1,14 @@
+﻿namespace Game
+{
+	public sealed class PlayerService
+	{
+		public Player Player => _player;
+
+		private readonly Player _player;
+
+		public PlayerService(Player player)
+		{
+			_player = player;
+		}
+	}
+}
