15f6c96e1efa46f55a5934659160b41ad1e886cf
FuryOhFury <needsomespaceforit2@gmail.com>
2025-05-25 20:42:58 +0500

    feat/add PikminControlComponent

---
+++ Assets/Game/GameEngine/Components/Content/IPikminControl/PikminControlComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game
+{
+	[Serializable]
+	public sealed class PikminControlComponent
+	{
+		[ShowInInspector]
+		private HashSet<GameObject> _pikmins = new();
+
+		[Button]
+		public void AddPikmin(GameObject pikmin)
+		{
+			_pikmins.Add(pikmin);
+		}
+
+		[Button]
+		public void RemovePikmin(GameObject pikmin)
+		{
+			_pikmins.Remove(pikmin);
+		}
+
+		public void SetTargetToPikmins(GameObject target, bool isPlayer)
+		{
+			foreach (var pikmin in _pikmins.ToArray())
+			{
+				if (pikmin.TryGetComponent(out IPikminTarget iPikminTarget) == false)
+				{
+					throw new NullReferenceException($"No IPikminTarget on {pikmin.name}");
+				}
+
+				if (iPikminTarget.TrySetTarget(target) && !isPlayer)
+				{
+					RemovePikmin(pikmin);
+				}
+			}
+		}
+	}
+}
