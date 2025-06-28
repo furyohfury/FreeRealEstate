013fb62e273fd1be78a845e26258d56d08f32e16
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:13:59 +0500

    feat/add billboard script for world ui

---
+++ Assets/Game/UI/Billboard.cs
@@ -0,0 +1,32 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+	public sealed class Billboard : MonoBehaviour
+	{
+		[ShowInInspector] [ReadOnly]
+		private Camera _targetCamera;
+
+		[Inject]
+		private void Construct(Camera camera)
+		{
+			_targetCamera = camera;
+		}
+
+		private void Start()
+		{
+			if (_targetCamera == null)
+			{
+				_targetCamera = Camera.main; // используем основную камеру, если не указана явно
+			}
+		}
+
+		private void LateUpdate()
+		{
+			Vector3 direction = transform.position - _targetCamera.transform.position;
+			transform.rotation = Quaternion.LookRotation(direction);
+		}
+	}
+}
