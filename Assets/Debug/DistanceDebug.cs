1097cd0fd306639d2ca0e97d3f8a75c032ec007e
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 22:59:00 +0500

    debug/add distance and SimpleCarriableEntity

---
+++ Assets/Debug/DistanceDebug.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+	public sealed class DistanceDebug : MonoBehaviour
+	{
+		[SerializeField] 
+		private float _radius = 1f;
+		[SerializeField] 
+		private float _distance = 1f;
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.color = Color.yellow;
+			var endPos = transform.position + transform.forward * _distance;
+			Gizmos.DrawLine(transform.position, endPos);
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere(endPos, _radius);
+		}
+	}
+}
