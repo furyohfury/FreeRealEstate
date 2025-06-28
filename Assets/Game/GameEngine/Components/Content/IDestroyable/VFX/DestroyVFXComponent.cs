5ba82240b2c0b46a556509cd1234db84baea1fdb
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:16:51 +0500

    feat/add DestroyVFXComponent

---
+++ Assets/Game/GameEngine/Components/Content/IDestroyable/VFX/DestroyVFXComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace GameEngine
+{
+	[Serializable]
+	public sealed class DestroyVFXComponent
+	{
+		[SerializeField]
+		private ParticleSystem _vfx;
+		[SerializeField]
+		private Transform _spawnPosition;
+
+		public void SpawnVFX()
+		{
+			Object.Instantiate(_vfx, _spawnPosition);
+		}
+	}
+}
