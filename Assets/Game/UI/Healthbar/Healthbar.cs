b4b8a773db26b861ee1be35a08ecd640461fc6bd
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:13:01 +0500

    feat/add healthbar ui

---
+++ Assets/Game/UI/Healthbar/Healthbar.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+	public sealed class HealthBar : MonoBehaviour
+	{
+		[SerializeField]
+		private Image _bar;
+		[SerializeField] 
+		private float _animationDuration = 0.2f;
+		private TweenerCore<float,float,FloatOptions> _tween;
+
+		public void SetBar(float ratio)
+		{
+			_tween?.Kill();
+			ratio = Mathf.Clamp01(ratio);
+			_tween = DOTween.To(
+				                () => _bar.fillAmount,
+				                x => _bar.fillAmount = x,
+				                ratio,
+				                _animationDuration
+			                )
+			                .SetEase(Ease.OutCubic);
+		}
+		
+		private void OnDestroy()
+		{
+			_tween.Kill();
+		}
+	}
+}
---
+++ Assets/Game/UI/Healthbar/HealthbarUIComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Game;
+using UnityEngine;
+
+namespace GameEngine
+{
+	[Serializable]
+	public class HealthbarUIComponent
+	{
+		[SerializeField]
+		private HealthBar _healthBar;
+
+		public void SetRatio(float ratio)
+		{
+			_healthBar.SetBar(ratio);
+		}
+
+		public void Hide()
+		{
+			_healthBar.gameObject.SetActive(false);
+		}
+	}
+}
