c5cdaaafbe7506ba80d8d61196195e12617d026b
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:13:43 +0500

    feat/add carriable ui

---
+++ Assets/Game/UI/Carriable/CarriableUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+	public sealed class CarriableUI : MonoBehaviour
+	{
+		[SerializeField]
+		private Image _bar;
+		[SerializeField]
+		private TMP_Text _text;
+		[SerializeField]
+		private float _animationDuration = 0.2f;
+		private TweenerCore<float, float, FloatOptions> _tween;
+
+		public void SetCount(string text)
+		{
+			_text.text = text;
+		}
+
+		public void SetBar(float ratio)
+		{
+			_tween?.Kill();
+			_tween = DOTween.To(
+				                () => _bar.fillAmount,
+				                x => _bar.fillAmount = x,
+				                ratio,
+				                _animationDuration
+			                )
+			                .SetEase(Ease.OutCubic);
+		}
+
+		private void OnDestroy()
+		{
+			_tween.Kill();
+		}
+	}
+}
---
+++ Assets/Game/UI/Carriable/CarriableUIComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+	[Serializable]
+	public sealed class CarriableUIComponent
+	{
+		[SerializeField]
+		private CarriableUI _carriableUI;
+
+		public void SetAmount(int current, int needed)
+		{
+			_carriableUI.SetCount($"{current}/{needed}");
+			_carriableUI.SetBar((float)current / needed);
+		}
+	}
+}
