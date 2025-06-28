4a854c5ec3f13215b9be11776beb6f546358b562
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:15:14 +0500

    feat/add TakeDamageStateChangeComponent

---
+++ Assets/Game/GameEngine/Components/Content/ILife/TakeDamageStateChangeComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameEngine
+{
+	[Serializable]
+	public sealed class TakeDamageStateChangeComponent
+	{
+		[SerializeField]
+		private GameObject[] _stateObjects;
+		private int _stateObjectIndex = 0;
+
+		public void ChangeState()
+		{
+			if (_stateObjectIndex >= _stateObjects.Length - 1)
+			{
+				return;
+			}
+
+			_stateObjects[_stateObjectIndex++].SetActive(false);
+			_stateObjects[_stateObjectIndex].SetActive(true);
+		}
+	}
+}
