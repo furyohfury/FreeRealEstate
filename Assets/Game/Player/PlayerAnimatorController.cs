0b1e8abd1aa232eacea77f4d90c160d5fb873634
FuryOhFury <needsomespaceforit2@gmail.com>
2025-06-28 14:16:15 +0500

    feat/add PlayerAnimatorController

---
+++ Assets/Game/Player/PlayerAnimatorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using GameEngine;
+using R3;
+using UnityEngine;
+using Zenject;
+
+namespace Game
+{
+	public sealed class PlayerAnimatorController : IInitializable, IDisposable
+	{
+		private readonly PlayerService _playerService;
+		private Animator _playerAnimator;
+		private readonly PlayerInputReader _playerInputReader;
+		private readonly CompositeDisposable _disposable = new();
+
+		public PlayerAnimatorController(PlayerService playerService, PlayerInputReader playerInputReader)
+		{
+			_playerService = playerService;
+			_playerInputReader = playerInputReader;
+		}
+
+		public void Initialize()
+		{
+			_playerAnimator = _playerService.Player.Animator;
+
+			_playerInputReader.OnGatherAction
+			                  .Subscribe(_ => _playerAnimator.SetTrigger(AnimatorHash.Gather))
+			                  .AddTo(_disposable);
+
+			_playerInputReader.OnInteractAction
+			                  .Subscribe(_ => _playerAnimator.SetTrigger(AnimatorHash.Interact))
+			                  .AddTo(_disposable);
+		}
+
+		public void Dispose()
+		{
+			_disposable.Dispose();
+		}
+	}
+}
