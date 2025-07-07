5478744de04a98421ee9c4710366ad801e9f9ed1
FuryOhFury <needsomespaceforit2@gmail.com>
2025-07-07 18:56:12 +0500

    chore/light and scene settings

---
+++ Assets/Debug/EditorOnlyFinderDebug.cs
@@ -0,0 +1,18 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game
+{
+	public sealed class EditorOnlyFinderDebug : MonoBehaviour
+	{
+		[Button]
+		private void LogEditorOnlyObjects()
+		{
+			GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("EditorOnly");
+			for (int i = 0, count = gameObjects.Length; i < count; i++)
+			{
+				Debug.Log(gameObjects[i].name);
+			}
+		}
+	}
+}
