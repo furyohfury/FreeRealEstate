cf1d0469309f8284613196fc9c57ceded92b085e
FuryOhFury <needsomespaceforit2@gmail.com>
2025-07-26 12:52:27 +0500

    feat/add score ui

---
+++ Assets/Game/UI/Common/TextView.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+
+namespace Game.UI
+{
+	public sealed class TextView : MonoBehaviour
+	{
+		[SerializeField] [Required]
+		private TMP_Text _textField;
+
+		public void SetText(string text)
+		{
+			_textField.SetText(text);
+		}
+	}
+}
---
+++ Assets/Game/UI/Score/ScorePresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Game.Scoring;
+using R3;
+using VContainer.Unity;
+
+namespace Game.UI
+{
+	public sealed class ScorePresenter : IStartable, IDisposable
+	{
+		private readonly MapScore _mapScore;
+		private readonly TextView _scoreTextView;
+		private readonly SerialDisposable _disposable = new();
+
+		public ScorePresenter(MapScore mapScore, TextView scoreTextView)
+		{
+			_mapScore = mapScore;
+			_scoreTextView = scoreTextView;
+		}
+
+		public void Start()
+		{
+			_disposable.Disposable = _mapScore.Score
+			                                  .Subscribe(OnScoreChanged);
+		}
+
+		private void OnScoreChanged(int score)
+		{
+			_scoreTextView.SetText(score.ToString());
+		}
+
+		public void Dispose()
+		{
+			_disposable.Dispose();
+		}
+	}
+}
