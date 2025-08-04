a2b1c89af5ea1fdb316a0af32335227640003aad
FuryOhFury <needsomespaceforit2@gmail.com>
2025-08-04 14:55:39 +0500

    feat/add services

---
+++ Assets/Game/Services/DrumrollTickrateService.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Game.ElementHandle
+{
+	public sealed class DrumrollTickrateService
+	{
+		private readonly DrumrollTickRateConfig _tickRateConfig;
+
+		public DrumrollTickrateService(DrumrollTickRateConfig tickRateConfig)
+		{
+			_tickRateConfig = tickRateConfig;
+		}
+
+		public float GetTickrate(int bpm)
+		{
+			return _tickRateConfig.GetTickRate(bpm);
+		}
+
+		public int GetNotesCountByDuration(int bpm, float drumrollDuration)
+		{
+			var tickRate = GetTickrate(bpm);
+			var beatDuration = (float)60 / bpm;
+			var tickInterval = beatDuration / tickRate;
+			var noteCount = drumrollDuration / tickInterval;
+			if (noteCount % 1 != 0)
+			{
+				Debug.LogWarning($"Drumroll duration must be dividable by {tickInterval}");
+			}
+
+			var drumRollNoteCount = Mathf.FloorToInt(noteCount);
+			return drumRollNoteCount;
+		}
+	}
+}
---
+++ Assets/Game/Services/NotesLineBoundsService.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Game.Services
+{
+	public sealed class NotesLineBoundsService
+	{
+		public Vector2 StartPoint => _startPoint.position;
+		public Vector2 HitPoint => _hitPoint.position;
+		public Transform NotesContainer => _notesContainer;
+		private readonly Transform _startPoint;
+		private readonly Transform _hitPoint;
+		private readonly Transform _notesContainer;
+
+		public NotesLineBoundsService(Transform startPoint, Transform hitPoint, Transform notesContainer)
+		{
+			_startPoint = startPoint;
+			_hitPoint = hitPoint;
+			_notesContainer = notesContainer;
+		}
+
+		public float GetNoteLineDistance()
+		{
+			return Vector2.Distance(HitPoint, StartPoint);
+		}
+	}
+}
