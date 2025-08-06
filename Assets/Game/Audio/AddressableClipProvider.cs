3635be9b42d0226baf4f26a331553909b95085d7
FuryOhFury <needsomespaceforit2@gmail.com>
2025-08-06 21:25:57 +0500

    feat/add AddressableClipProvider for audio

---
+++ Assets/Game/Audio/AddressableClipProvider.cs
@@ -0,0 +1,30 @@
+﻿using Audio;
+using Cysharp.Threading.Tasks;
+using ObjectProvide;
+using UnityEngine;
+
+namespace Game.Audio
+{
+	public class AddressableClipProvider : IAudioClipProvider
+	{
+		private readonly IObjectProvider _assetProvider;
+		private readonly AudioLibrary _audioIdLibrary;
+
+		public AddressableClipProvider(IObjectProvider assetProvider, AudioLibrary audioIdLibrary)
+		{
+			_assetProvider = assetProvider;
+			_audioIdLibrary = audioIdLibrary;
+		}
+
+		public async UniTask<AudioClip> GetClipAsync(string clipName)
+		{
+			if (_audioIdLibrary.TryGetClipIdByName(clipName, out string clipId))
+			{
+				return await _assetProvider.Get<AudioClip>(clipId);
+			}
+
+			Debug.LogError($"Clip ID for name '{clipName}' not found in AudioLibrary.");
+			return null;
+		}
+	}
+}
