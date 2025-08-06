8175870f5e63340069951fe05c7010e0628b58b8
FuryOhFury <needsomespaceforit2@gmail.com>
2025-08-06 21:26:57 +0500

    refac/installers

--- Assets/Installers/CoreInstaller.cs
+++ Assets/Installers/CoreInstaller.cs
@@ -1,4 +1,9 @@
-﻿using Game.BeatmapControl;
+﻿using Game;
+using Game.BeatmapControl;
+using Game.BeatmapLaunch;
+using Game.ElementHandle;
+using Game.Input;
+using Game.Scoring;
 using Game.Services;
 using Game.SongMapTime;
 using Game.Visuals;
@@ -9,8 +14,15 @@ using VContainer.Unity;
 
 namespace Installers
 {
-	public sealed class VisualsInstaller : LifetimeScope
+	public class CoreInstaller : LifetimeScope
 	{
+		[Header("Core")]
+		[SerializeField]
+		private PointsForStatusConfig _pointsForStatusConfig;
+		[SerializeField]
+		private DrumrollTickRateConfig _drumrollTickrateConfig;
+
+		[Header("Visuals")]
 		[SerializeField] [Required]
 		private SingleNotePrefabConfig _singleNotePrefabConfig;
 		[SerializeField] [Required]
@@ -29,6 +41,62 @@ namespace Installers
 		private ActiveSpinnerView _activeSpinnerViewPrefab;
 
 		protected override void Configure(IContainerBuilder builder)
+		{
+			builder.Register<MapTime>(Lifetime.Singleton)
+			       .AsImplementedInterfaces()
+			       .AsSelf();
+			builder.Register<BeatmapPipeline>(Lifetime.Singleton);
+
+			builder.RegisterEntryPoint<SingleNoteTimeoutSwitcher>();
+			builder.RegisterEntryPoint<SpinnerTimeoutSwitcher>();
+			builder.RegisterEntryPoint<ElementOnStatusSwitcher>();
+			builder.RegisterEntryPoint<DrumrollTimeoutSwitcher>();
+
+			builder.Register<ElementClickStrategy, SingleNoteClickStrategy>(Lifetime.Scoped);
+			builder.Register<ElementClickStrategy, SpinnerClickStrategy>(Lifetime.Scoped);
+			builder.Register<DrumrollTickrateService>(Lifetime.Singleton);
+			builder.RegisterInstance<DrumrollTickRateConfig>(_drumrollTickrateConfig);
+			builder.Register<ElementClickStrategy, DrumrollClickStrategy>(Lifetime.Scoped);
+			builder.Register<ElementsClickHandler>(Lifetime.Singleton);
+			builder.Register<DifficultySwitcher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces()
+			       .AsSelf();
+
+			builder.Register<InputReader>(Lifetime.Singleton)
+			       .AsImplementedInterfaces()
+			       .AsSelf();
+			builder.Register<InputActions>(Lifetime.Singleton);
+
+			builder.RegisterEntryPoint<InputNotesController>();
+
+			builder.Register<MapScore>(Lifetime.Singleton);
+			builder.RegisterEntryPoint<MapScoreController>();
+			builder.RegisterEntryPoint<MapScoreResetter>();
+			builder.RegisterInstance<PointsForStatusConfig>(_pointsForStatusConfig);
+
+			RegisterLaunchers(builder);
+
+			Debug.Log("Successfully installed all scene systems");
+
+			RegisterVisualSystems(builder);
+		}
+
+		private static void RegisterLaunchers(IContainerBuilder builder)
+		{
+			builder.Register<BeatmapTimeLauncher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+			builder.Register<BeatmapAudioLauncher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+			builder.Register<BeatmapBackgroundLauncher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+			builder.Register<BeatmapElementsVisualLauncher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+			builder.Register<BeatmapPipelineLauncher>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+			builder.Register<BeatmapLauncher>(Lifetime.Singleton);
+		}
+
+		private void RegisterVisualSystems(IContainerBuilder builder)
 		{
 			builder.RegisterInstance<NotesLineBoundsService>(new NotesLineBoundsService(_startPoint, _endPoint, _notesContainer));
 			builder.Register<NotesLineMover>(Lifetime.Singleton)
---
+++ Assets/Installers/ProjectInstaller.cs
@@ -0,0 +1,43 @@
+﻿using Audio;
+using Game.Audio;
+using ObjectProvide;
+using UnityEngine;
+using UnityEngine.Audio;
+using VContainer;
+using VContainer.Unity;
+
+namespace Installers
+{
+	public sealed class ProjectInstaller : LifetimeScope
+	{
+		[SerializeField]
+		private AudioLibrary _audioLibrary;
+		[SerializeField]
+		private AudioLayerSetting _audioLayerSetting;
+		[SerializeField]
+		private AudioMixer _audioMixer;
+
+		protected override void Configure(IContainerBuilder builder)
+		{
+			InstallObjectProvider(builder);
+			InstallAudioSystem(builder);
+		}
+
+		private static void InstallObjectProvider(IContainerBuilder builder)
+		{
+			builder.Register<AddressablesObjectProvider>(Lifetime.Singleton)
+			       .AsImplementedInterfaces();
+		}
+
+		private void InstallAudioSystem(IContainerBuilder builder)
+		{
+			builder.RegisterInstance<AudioLibrary>(_audioLibrary);
+			builder.RegisterInstance<AudioLayerSetting>(_audioLayerSetting);
+			builder.RegisterInstance<AudioMixer>(_audioMixer);
+			builder.Register<IAudioClipProvider, AddressableClipProvider>(Lifetime.Singleton);
+			builder.Register<AudioManager>(Lifetime.Singleton)
+			       .AsImplementedInterfaces()
+			       .AsSelf();
+		}
+	}
+}
