d017206fa00b821648513a0ee8b760e62972c26f
FuryOhFury <needsomespaceforit2@gmail.com>
2025-08-06 21:25:31 +0500

    refac/beadmap bundle

--- Assets/Game/BeatmapBundle/BeatmapBundle.cs
+++ Assets/Game/BeatmapBundle/BeatmapBundle.cs
@@ -1,29 +1,42 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Beatmaps;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-namespace Game.BeatmapBundle
+namespace Game.BeatmapBundles
 {
-	[CreateAssetMenu(fileName = "BeatmapBundle", menuName = "BeatmapBundle")]
-	public sealed class BeatmapBundle : SerializedScriptableObject
+	[CreateAssetMenu(fileName = "BeatmapBundle", menuName = "Beatmap/BeatmapBundle")]
+	public sealed class BeatmapBundle : ScriptableObject
 	{
+		public string Name => _name;
+		public string SongId => _songId;
+		public string BackgroundId => _backgroundId;
+		public BeatmapVariant[] BeatmapsVariants => _beatmapsVariants;
+
+		public BeatmapBundle(string name, string songId, string backgroundId, BeatmapVariant[] beatmapsVariants)
+		{
+			_name = name;
+			_songId = songId;
+			_backgroundId = backgroundId;
+			_beatmapsVariants = beatmapsVariants;
+		}
+
 		[SerializeField]
 		private string _name;
 		[SerializeField]
-		private AudioClip _song;
+		private string _songId;
 		[SerializeField]
-		private Sprite _background;
+		private string _backgroundId;
 		[SerializeField]
 		private BeatmapVariant[] _beatmapsVariants;
 
-		[Serializable]
-		private struct BeatmapVariant
+		[Button]
+		private void AddVariant(BeatmapConfig beatmapConfig, float startTime, float endTime)
 		{
-			public BeatmapConfig BeatmapConfig;
-			public DifficultyConfig DifficultyConfig;
-			public float SongStartTimeInSeconds;
-			public float SongEndTimeInSeconds;
+			List<BeatmapVariant> beatmapVariants = _beatmapsVariants.ToList();
+			beatmapVariants.Add(new BeatmapVariant(beatmapConfig, startTime, endTime));
+			_beatmapsVariants = beatmapVariants.ToArray();
 		}
 	}
 }
---
+++ Assets/Game/BeatmapBundle/BeatmapVariant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Beatmaps;
+
+namespace Game.BeatmapBundles
+{
+	[Serializable]
+	public sealed class BeatmapVariant
+	{
+		public BeatmapConfig BeatmapConfig;
+		public float SongStartTimeInSeconds;
+		public float SongEndTimeInSeconds;
+
+		public BeatmapVariant(BeatmapConfig beatmapConfig, float songStartTimeInSeconds, float songEndTimeInSeconds)
+		{
+			BeatmapConfig = beatmapConfig;
+			SongStartTimeInSeconds = songStartTimeInSeconds;
+			SongEndTimeInSeconds = songEndTimeInSeconds;
+		}
+	}
+}
