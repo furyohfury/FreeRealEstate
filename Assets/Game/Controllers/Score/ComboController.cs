3dd33abd232367109967ff8a4a01310703e85194
FuryOhFury <needsomespaceforit2@gmail.com>
2025-08-21 17:00:50 +0500

    feat/add ComboController and presenter

---
+++ Assets/Game/Controllers/Score/ComboController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Game.ElementHandle;
+using Game.Scoring;
+using R3;
+using VContainer.Unity;
+
+namespace Game.Controllers
+{
+	public sealed class ComboController : IInitializable, IDisposable
+	{
+		private readonly ScoreSystem _scoreSystem;
+		private readonly IHandleResultObservable _handleResultObservable;
+		private IDisposable _disposable;
+
+		public ComboController(IHandleResultObservable handleResultObservable, ScoreSystem scoreSystem)
+		{
+			_handleResultObservable = handleResultObservable;
+			_scoreSystem = scoreSystem;
+		}
+
+		public void Initialize()
+		{
+			_disposable = _handleResultObservable.OnElementHandled
+			                                     .Subscribe(OnNoteHit);
+		}
+
+		private void OnNoteHit(HandleResult result)
+		{
+			if (result is MissHandleResult)
+			{
+				_scoreSystem.ResetCombo();
+			}
+			else
+			{
+				_scoreSystem.AddCombo();
+			}
+		}
+
+		public void Dispose()
+		{
+			_disposable.Dispose();
+		}
+	}
+}
---
+++ Assets/Game/UI/Score/ComboPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Game.Scoring;
+using R3;
+using VContainer.Unity;
+
+namespace Game.UI
+{
+	public sealed class ComboPresenter : IInitializable, IDisposable
+	{
+		private readonly TextView _comboTextView;
+		private readonly ScoreSystem _scoreSystem;
+		private IDisposable _disposable;
+
+		public ComboPresenter(TextView comboTextView, ScoreSystem scoreSystem)
+		{
+			_comboTextView = comboTextView;
+			_scoreSystem = scoreSystem;
+		}
+
+		public void Initialize()
+		{
+			_disposable = _scoreSystem.Combo
+			                          .Subscribe(OnComboChanged);
+		}
+
+		private void OnComboChanged(int score)
+		{
+			_comboTextView.SetText(score.ToString());
+		}
+
+		public void Dispose()
+		{
+			_disposable.Dispose();
+		}
+	}
+}
