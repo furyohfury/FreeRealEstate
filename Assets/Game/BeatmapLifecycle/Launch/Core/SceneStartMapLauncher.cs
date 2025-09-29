6b93ed6272968db9af8165cb743b130d9b8b626a
FuryOhFury <needsomespaceforit2@gmail.com>
2025-09-29 12:33:36 +0500

    feat/add starting map in gameplay scene

---
+++ Assets/Game/BeatmapLifecycle/Launch/Core/SceneStartMapLauncher.cs
@@ -0,0 +1,25 @@
+﻿using Cysharp.Threading.Tasks;
+using VContainer.Unity;
+
+namespace Game.BeatmapLaunch
+{
+	public sealed class SceneStartMapLauncher : IPostStartable
+	{
+		private readonly BeatmapLauncher _beatmapLauncher;
+		private readonly CurrentBundleService _currentBundleService;
+
+		public SceneStartMapLauncher(BeatmapLauncher beatmapLauncher, CurrentBundleService currentBundleService)
+		{
+			_beatmapLauncher = beatmapLauncher;
+			_currentBundleService = currentBundleService;
+		}
+
+		public void PostStart()
+		{
+			if (_currentBundleService.HasActiveBeatmap)
+			{
+				_beatmapLauncher.Launch(_currentBundleService.CurrentBundle, _currentBundleService.CurrentVariant).Forget();
+			}
+		}
+	}
+}
