cfa4e7efb38af7373a801161295b5725d1e1b6ae
FuryOhFury <needsomespaceforit2@gmail.com>
2025-09-29 12:35:41 +0500

    func/add AuthSceneSwitchable

---
+++ Assets/Game/Modules/SceneSwitch/AuthSceneSwitchable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.AddressableAssets;
+
+namespace Game.SceneSwitch
+{
+	public sealed class AuthSceneSwitchable : ISceneSwitchable
+	{
+		public void SwitchScene()
+		{
+			var scene = Scenes.AuthScene.ToString();
+			ISceneSwitchable.OnSceneSwitched?.Invoke(scene);
+			Addressables.LoadSceneAsync(scene);
+		}
+	}
+}
