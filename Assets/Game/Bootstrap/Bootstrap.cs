9b723e3f0856056b6b5ec66e74f7e6a0531204ba
FuryOhFury <needsomespaceforit2@gmail.com>
2025-09-29 12:39:40 +0500

    feat/add bootstrap

---
+++ Assets/Game/Bootstrap/Bootstrap.cs
@@ -0,0 +1,22 @@
+﻿using Game.SceneSwitch;
+using UnityEngine;
+using VContainer;
+
+namespace Game.Bootstrap
+{
+	public sealed class Bootstrap : MonoBehaviour
+	{
+		private ISceneSwitchable _sceneSwitchable;
+
+		[Inject]
+		public void Construct(ISceneSwitchable sceneSwitchable)
+		{
+			_sceneSwitchable = sceneSwitchable;
+		}
+
+		private void Start()
+		{
+			_sceneSwitchable.SwitchScene();
+		}
+	}
+}
