ecadd5c475ecea72c8c7addc7a5ec983b2dc142c
FuryOhFury <needsomespaceforit2@gmail.com>
2025-09-29 12:32:52 +0500

    feat/disable music on scene switch

---
+++ Assets/Game/Controllers/Audio/AudioOnSceneSwitchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Audio;
+using Game.SceneSwitch;
+using VContainer.Unity;
+
+namespace Game.Controllers.Audio
+{
+	public sealed class AudioOnSceneSwitchController : IInitializable, IDisposable
+	{
+		private readonly AudioManager _audioManager;
+
+		public AudioOnSceneSwitchController(AudioManager audioManager)
+		{
+			_audioManager = audioManager;
+		}
+
+		public void Initialize()
+		{
+			ISceneSwitchable.OnSceneSwitched += OnSceneSwitched;
+		}
+
+		private void OnSceneSwitched(string obj)
+		{
+			_audioManager.StopAudioOutput(AudioOutput.Music);
+		}
+
+		public void Dispose()
+		{
+			ISceneSwitchable.OnSceneSwitched -= OnSceneSwitched;
+		}
+	}
+}
