c22c8d4ad6c9d6206ab4565d48018bae29fe9f65
FuryOhFury <needsomespaceforit2@gmail.com>
2025-10-12 15:06:11 +0500

    feat/add score zone

---
+++ Assets/Debug/ScaleDebugHelper.cs
@@ -0,0 +1,31 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Debug
+{
+	public sealed class ScaleDebugHelper : MonoBehaviour
+	{
+		[SerializeField]
+		private Transform[] _transforms;
+		[SerializeField]
+		private float _mult;
+
+		[Button]
+		private void Descale()
+		{
+			foreach (var t in _transforms)
+			{
+				t.localScale /= _mult;
+			}
+		}
+		
+		[Button]
+		private void Scale()
+		{
+			foreach (var t in _transforms)
+			{
+				t.localScale *= _mult;
+			}
+		}
+	}
+}
