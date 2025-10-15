f56757db905c3415ee621192c278c11e87c3b95e
FuryOhFury <needsomespaceforit2@gmail.com>
2025-10-15 15:27:47 +0500

    feat/every player has his camera pos

---
+++ Assets/Game/Gameplay/Camera/CameraPositioner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Unity.Netcode;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Gameplay.Camera
+{
+	public sealed class CameraPositioner : IInitializable, IDisposable
+	{
+		private readonly UnityEngine.Camera _camera;
+		private readonly Transform _playerOnePosition;
+		private readonly Transform _playerTwoPosition;
+
+		public CameraPositioner(UnityEngine.Camera camera, Transform playerOnePosition, Transform playerTwoPosition)
+		{
+			_camera = camera;
+			_playerOnePosition = playerOnePosition;
+			_playerTwoPosition = playerTwoPosition;
+		}
+
+		public void Initialize()
+		{
+			NetworkManager.Singleton.OnClientStarted += OnClientStarted;
+		}
+
+		private void OnClientStarted()
+		{
+			SetCameraPosition(NetworkManager.Singleton.IsHost
+				? _playerOnePosition
+				: _playerTwoPosition);
+		}
+
+		private void SetCameraPosition(Transform position)
+		{
+			_camera.transform.SetParent(position);
+			_camera.transform.localPosition = Vector3.zero;
+			_camera.transform.localRotation = Quaternion.identity;
+		}
+
+		public void Dispose()
+		{
+			NetworkManager.Singleton.OnClientStarted -= OnClientStarted;
+		}
+	}
+}
---
+++ Assets/Game/Network/CameraInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Game.Gameplay.Camera;
+using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+	public sealed class CameraInstaller : MonoInstaller
+	{
+		[SerializeField]
+		private Transform _playerOneCameraPos;
+		[SerializeField]
+		private Transform _playerTwoCameraPos;
+
+		public override void InstallBindings()
+		{
+			Container.Bind<Camera>()
+			         .FromInstance(Camera.main)
+			         .AsSingle();
+
+			Container.BindInterfacesTo<CameraPositioner>()
+			         .FromInstance(new CameraPositioner(Camera.main, _playerOneCameraPos, _playerTwoCameraPos))
+			         .AsSingle();
+		}
+	}
+}
