e892e8fa75817b25cc381ead19ee860823db553c
furyohfury <artemkuzminikh@gmail.com>
2026-04-01 17:32:03 +0500

    ui anims

--- Assets/Game/UI/DifficultySelectionUI.cs
+++ Assets/Game/UI/DifficultySelectionUI.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
+using TriInspector;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,6 +15,24 @@ namespace Game
         [SerializeField]
         private ButtonUI _buttonPrefab;
         private readonly Dictionary<ButtonUI, int> _buttonToParamsMap = new Dictionary<ButtonUI, int>();
+        [Header("Settings")]
+        [SerializeField]
+        private float _fallAnimDuration = 0.3f;
+        [SerializeField]
+        private float _fallScalePunchDuration = 0.3f;
+        [SerializeField]
+        private Vector3 _punchScaleStrength = new Vector3(0.3f, -0.2f, 0);
+        [SerializeField]
+        private float _punchDuration = 0.5f;
+
+        private RectTransform _rectTransform;
+        private Vector2 _originalAnchoredPosition;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _originalAnchoredPosition = _rectTransform.anchoredPosition;
+        }
 
         private void Start()
         {
@@ -20,7 +40,7 @@ namespace Game
             {
                 Destroy(child.gameObject);
             }
-            
+
             SessionParams[] sessionParams = _sessionParamsStorage.SessionParams;
 
             for (int i = 0, count = sessionParams.Length; i < count; i++)
@@ -30,6 +50,25 @@ namespace Game
                 button.OnClick += OnButtonClicked;
                 _buttonToParamsMap.Add(button, i);
             }
+
+            LaunchAppearanceAnimation();
+        }
+
+        [Button]
+        private void LaunchAppearanceAnimation()
+        {
+            DOTween.Kill(_rectTransform);
+            _rectTransform.localScale = Vector3.one;
+            float screenHeight = Screen.height;
+            float halfHeight = _rectTransform.rect.height / 2;
+            _rectTransform.anchoredPosition = new Vector2(_originalAnchoredPosition.x, screenHeight + halfHeight);
+            Sequence s = DOTween.Sequence();
+
+            s.Append(_rectTransform.DOAnchorPos(_originalAnchoredPosition, _fallAnimDuration).SetEase(Ease.InQuad));
+            s.Append(_rectTransform.DOPunchScale(_punchScaleStrength, _punchDuration, 10, 1f));
+            s.Join(_rectTransform.DOAnchorPosY(_originalAnchoredPosition.y + 15f, _punchDuration / 2)
+                                 .SetLoops(2, LoopType.Yoyo)
+                                 .SetEase(Ease.OutQuad));
         }
 
         private void OnButtonClicked(ButtonUI buttonUI)
@@ -37,7 +76,7 @@ namespace Game
             buttonUI.OnClick -= OnButtonClicked;
             int paramsIndex = _buttonToParamsMap[buttonUI];
             GameParamsService.Instance.SessionParams = _sessionParamsStorage.SessionParams[paramsIndex];
-            SceneManager.LoadScene((int) Scene.Gameplay, LoadSceneMode.Single);
+            SceneManager.LoadScene((int)Scene.Gameplay, LoadSceneMode.Single);
         }
     }
 }
