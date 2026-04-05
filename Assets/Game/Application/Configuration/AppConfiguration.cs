00460e039ff68e5cc8b993134b2c43adffc29b31
FuryOhFury <needsomespaceforit2@gmail.com>
2026-04-05 23:20:20 +0500

    add app config

---
+++ Assets/Game/Application/Configuration/AppConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game.Application
+{
+    public abstract class AppConfiguration : ScriptableObject
+    {
+        public abstract float GetMaxHealth();
+        public abstract bool GetTrackDeath();
+        public abstract float GetMusicVolumeMult();
+    }
+}
---
+++ Assets/Game/Application/Configuration/DefaultAppConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game.Application
+{
+    [CreateAssetMenu(fileName = "DefaultAppConfiguration", menuName = "Game/AppConfig/DefaultAppConfiguration")]
+    public sealed class DefaultAppConfiguration : AppConfiguration
+    {
+        [SerializeField]
+        private float _maxHealth = 100f;
+        [SerializeField]
+        private bool _trackDeath = false;
+        [SerializeField] [Range(0f, 1f)]
+        private float _musicVolumeMultiplier = 0.5f;
+
+        public override float GetMaxHealth()
+        {
+            return _maxHealth;
+        }
+
+        public override bool GetTrackDeath()
+        {
+            return _trackDeath;
+        }
+
+        public override float GetMusicVolumeMult()
+        {
+            return _musicVolumeMultiplier;
+        }
+    }
+}
