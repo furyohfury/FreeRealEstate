64f59f492f4adc08476a93c6e6dccf300a8f89dc
FuryOhFury <needsomespaceforit2@gmail.com>
2026-04-05 23:35:49 +0500

    yg flags

---
+++ Assets/Game/Application/Configuration/YGAppConfiguration.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using YG;
+
+namespace Game.Application
+{
+    [CreateAssetMenu(fileName = "YGAppConfiguration", menuName = "Game/AppConfig/YGAppConfiguration")]
+    public sealed class YGAppConfiguration : AppConfiguration
+    {
+        [SerializeField]
+        private float _maxHealth = 100f;
+        [SerializeField]
+        private bool _trackDeath = true;
+        [SerializeField] [Range(0f, 1f)]
+        private float _musicVolume = 0.25f;
+
+        public override float GetMaxHealth()
+        {
+            if (YG2.TryGetFlagAsFloat(YGAppConfigurationFlags.MAX_HP, out float value))
+            {
+                Debug.Log($"<color=green>Got flag {YGAppConfigurationFlags.MAX_HP} from YG</color>");
+                return value;
+            }
+
+            Debug.LogWarning($"Couldnt get flag {YGAppConfigurationFlags.MAX_HP} from YG. Taking default</color>");
+            return _maxHealth;
+        }
+
+        public override bool GetTrackDeath()
+        {
+            if (YG2.TryGetFlagAsBool(YGAppConfigurationFlags.TRACK_DEATH, out bool value))
+            {
+                Debug.Log($"<color=green>Got flag {YGAppConfigurationFlags.TRACK_DEATH} from YG</color>");
+                return value;
+            }
+
+            Debug.LogWarning($"Couldnt get flag {YGAppConfigurationFlags.TRACK_DEATH} from YG. Taking default</color>");
+            return _trackDeath;
+        }
+
+        public override float GetMusicVolumeMult()
+        {
+            if (YG2.TryGetFlagAsFloat(YGAppConfigurationFlags.MUSIC_VOLUME_MULT, out float value))
+            {
+                Debug.Log($"<color=green>Got flag {YGAppConfigurationFlags.MUSIC_VOLUME_MULT} from YG</color>");
+                return value;
+            }
+
+            Debug.LogWarning($"Couldnt get flag {YGAppConfigurationFlags.MUSIC_VOLUME_MULT} from YG. Taking default</color>");
+            return _musicVolume;
+        }
+    }
+}
---
+++ Assets/Game/Application/Configuration/YGAppConfigurationFlags.cs
@@ -0,0 +1,9 @@
+﻿namespace Game.Application
+{
+    public static class YGAppConfigurationFlags
+    {
+        public const string MAX_HP = "max_hp";
+        public const string TRACK_DEATH = "track_death";
+        public const string MUSIC_VOLUME_MULT = "music_volume_mult";
+    }
+}
