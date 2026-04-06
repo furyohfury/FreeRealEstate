7fd2f1a40d4c1ef4b8b8ac7436765ccadb2e3e4f
furyohfury <artemkuzminikh@gmail.com>
2026-04-06 15:34:21 +0500

    lb view fix

--- Assets/Game/LeaderboardPresenter.cs
+++ Assets/Game/LeaderboardPresenter.cs
@@ -1,37 +1,9 @@
-﻿using Game.Infrastructure;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Game
 {
     public abstract class LeaderboardPresenter : MonoBehaviour
     {
-        [SerializeField]
-        private ButtonUI _restartButton;
-        [SerializeField]
-        private ButtonUI _quitButton;
-
-        private void OnEnable()
-        {
-            _restartButton.OnClick += RestartButtonOnOnClick;
-            _quitButton.OnClick += QuitButtonOnOnClick;
-        }
-
-        private void RestartButtonOnOnClick(ButtonUI obj)
-        {
-            SessionRestarter.Instance.Restart();
-        }
-
-        private void QuitButtonOnOnClick(ButtonUI obj)
-        {
-            SceneSwitcher.Instance.SwitchSceneWithLoadingScreen(Scene.MainMenu);
-        }
-
-        private void OnDisable()
-        {
-            _restartButton.OnClick -= RestartButtonOnOnClick;
-            _quitButton.OnClick -= QuitButtonOnOnClick;
-        }
-
         public abstract void ShowLeaderboard();
         public abstract void HideLeaderboard();
     }
