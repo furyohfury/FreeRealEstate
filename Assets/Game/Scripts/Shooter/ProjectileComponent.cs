91e848e7da6615c3fa48e1c97e8f788e9966aea6
FuryOhFury <needsomespaceforit2@gmail.com>
2026-05-17 22:44:52 +0500

    shoot comp prep

---
+++ Assets/Game/Scripts/Shooter/ProjectileComponent.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+namespace Game.Scripts.Shooter
+{
+    public sealed class ProjectileComponent : NetworkBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+
+        private void FixedUpdate()
+        {
+            transform.Translate(Vector3.forward * (_speed * Time.deltaTime));
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Shooter/ShootComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts.Shooter
+{
+    public sealed class ShootComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _firePoint;
+        [SerializeField]
+        private GameObject _projectilePrefab;
+        [SerializeField]
+        private float _cooldown;
+        private float _timer;
+
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                _timer = _cooldown;
+                Shoot();
+            }
+        }
+
+        public void Shoot()
+        {
+            GameObject projectile = Instantiate(_projectilePrefab, _firePoint.position, Quaternion.Euler(transform.forward));
+        }
+    }
+}
