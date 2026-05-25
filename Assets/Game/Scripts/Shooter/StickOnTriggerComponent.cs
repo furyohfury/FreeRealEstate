db2b90a306473d89fdf45e925332763e33d48452
FuryOhFury <needsomespaceforit2@gmail.com>
2026-05-25 23:30:28 +0500

    foff

--- Assets/Game/Scripts/Shooter/StickOnTriggerComponent.cs
+++ Assets/Game/Scripts/Shooter/StickOnTriggerComponent.cs
@@ -7,8 +7,11 @@ namespace Game.Scripts.Shooter
     public sealed class StickOnTriggerComponent : NetworkBehaviour
     {
         [SerializeField]
-        private float _stickDistance = 0.3f;
+        private float _stickDistance = 0.1f;
+        [SerializeField]
         private bool _isCollided;
+        [SerializeField]
+        private Collider _collider;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -19,8 +22,20 @@ namespace Game.Scripts.Shooter
                 return;
             }
 
-            transform.position += transform.forward * _stickDistance;
             _isCollided = true;
+            Vector3 stickPosition = transform.position + (other.transform.position - transform.position).normalized * _stickDistance;
+            transform.position = stickPosition;
+            transform.SetParent(other.transform);
+            SpawnFakeArrowClientRpc(stickPosition, transform.rotation, other.transform); // todo передавать только номер кости и айди
+                                                                                         // networkobject'a сам. У себя тоже можно разрушить че
+                                                                                         // еще нужно дестроить с делеем
+            NetworkObject.Despawn();
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SpawnFakeArrowClientRpc(Vector3 position, Quaternion rotation, Transform parent)
+        {
+            Instantiate(gameObject, position, rotation, parent);
         }
     }
 }
